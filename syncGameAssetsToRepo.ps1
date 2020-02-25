8c089827ae80cca700f47ca6ff021fde035954bd
Kevin Ure <kevin.m.ure@gmail.com>
2020-02-24 20:25:20 -0500

    fixed the script

--- syncGameAssetsToRepo.ps1
+++ syncGameAssetsToRepo.ps1
@@ -3,7 +3,7 @@ $modssubdirectory = "Mods\Civilization 4-Ever\Assets"
 
 $basedir = (Join-Path $btsPath $modssubdirectory )
 
-Copy-Item (Join-Path $basedir "Art") -Destination (Join-Path $PSScriptRoot "game-assets") -Force
-Copy-Item (Join-Path $basedir "CvGameCoreDLL") -Destination (Join-Path $PSScriptRoot "game-assets") -Force
-Copy-Item (Join-Path $basedir "Python") -Destination (Join-Path $PSScriptRoot "game-assets") -Force
-Copy-Item (Join-Path $basedir "XML") -Destination (Join-Path $PSScriptRoot "game-assets") -Force
+Copy-Item (Join-Path $basedir "Art\*") -Destination (Join-Path $PSScriptRoot "game-assets\Art") -Force -Recurse
+Copy-Item (Join-Path $basedir "CvGameCoreDLL\*") -Destination (Join-Path $PSScriptRoot "game-assets\CvGameCoreDLL") -Force -Recurse
+Copy-Item (Join-Path $basedir "Python\*") -Destination (Join-Path $PSScriptRoot "game-assets\Python") -Force -Recurse
+Copy-Item (Join-Path $basedir "XML\*") -Destination (Join-Path $PSScriptRoot "game-assets\XML") -Force -Recurse
